dfeb04394a1e5918e2a82b8424d41a420c765769
terwer <youweics@163.com>
2023-05-05 23:33:40 +0800

    feat(zhi-lib-log): add a better log system

--- packages/zhi-lib-log/src/lib/logFactory.ts
+++ packages/zhi-lib-log/src/lib/logFactory.ts
@@ -23,8 +23,48 @@
  * questions.
  */
 
-describe("zhiLibLog", () => {
-  it("should work", () => {
-    console.log("hello")
-  })
-})
+import { Env } from "@siyuan-community/zhi-env"
+import DefaultLogger from "./defaultLogger"
+import LogLevelEnum from "./logConstants"
+import CustomLogFactory from "./factory/customLogFactory"
+
+/**
+ * 日志工具类
+ *
+ * @public
+ * @author terwer
+ * @since 1.0.7
+ */
+class LogFactory {
+  /**
+   * 默认日志记录器
+   *
+   * @param stackSize - 栈的深度
+   * @param env - 环境变量实例
+   */
+  public static defaultLogger(env?: Env, stackSize?: number): DefaultLogger {
+    return LogFactory.customLogFactory(undefined, undefined, env).getLogger(undefined, stackSize)
+  }
+
+  /**
+   * 自定义日志工厂
+   *
+   * @param level - 级别
+   * @param sign - 标志
+   * @param env - 环境变量
+   */
+  public static customLogFactory(level?: LogLevelEnum, sign?: string, env?: Env) {
+    return new CustomLogFactory(level, sign, env)
+  }
+
+  /**
+   * 自定义日志工厂，自定义前缀
+   *
+   * @param sign - 标志
+   * @param env - 环境变量
+   */
+  public static customSignLogFactory(sign?: string, env?: Env) {
+    return new CustomLogFactory(undefined, sign, env)
+  }
+}
+export default LogFactory
