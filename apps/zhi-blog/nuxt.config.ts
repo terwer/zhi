77b1485254abb34937988d5755b83a61206585cd
terwer <youweics@163.com>
2023-05-08 11:20:49 +0800

    chore(zhi-root): fix ci

--- apps/zhi-blog/nuxt.config.ts
+++ apps/zhi-blog/nuxt.config.ts
@@ -1,27 +1,60 @@
-const isDev = process.env.NODE_ENV === "development"
-const isSiyuanBuild = process.env.BUILD_TYPE === "siyuan"
-const isVercelBuild = process.env.BUILD_TYPE === "vercel"
+/*
+ * Copyright (c) 2023, Terwer . All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Terwer designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Terwer in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Terwer, Shenzhen, Guangdong, China, youweics@163.com
+ * or visit www.terwer.space if you need additional information or have any
+ * questions.
+ */
 
-const appBase = isSiyuanBuild
-  ? "/appearance/themes/zhi/web/blog/"
-  : isDev || isVercelBuild
-  ? "/"
-  : "/zhi/apps/zhi-web-blog/dist/"
-const distDir = isSiyuanBuild
-  ? "/Users/terwer/Documents/mydocs/SiYuanWorkspace/public/conf/appearance/themes/zhi/web/blog"
-  : "./dist"
-const isSsr = isDev || isVercelBuild
-// const ssrPreset = isVercelBuild ? "vercel" : isDev ? "node-server" : undefined
-// const ssrServeStatic = isSiyuanBuild
+const getAppBase = (isSiyuanBuild: boolean, isDev: boolean, isVercelBuild: boolean): string => {
+  if (isSiyuanBuild) {
+    return "/appearance/themes/zhi/blog/"
+  } else if (isNodeBuild || isVercelBuild) {
+    return "/"
+  } else {
+    // static
+    return "/zhi/apps/zhi-blog/dist/"
+  }
+}
+
+const isDev = process.env.NODE_ENV === "development"
+const isNodeBuild = process.env.BUILD_TYPE === "node"
+const isVercelBuild = process.env.BUILD_TYPE === "vercel"
+const isStaticBuild = process.env.BUILD_TYPE === "nginx"
+const isSiyuanBuild = process.env.BUILD_TYPE === "siyuan"
+
+const appBase = getAppBase(isSiyuanBuild, isDev, isVercelBuild)
+const isSsr = isNodeBuild || isVercelBuild
+const ssrPreset = isVercelBuild ? "vercel" : isDev ? "node-server" : undefined
+const ssrServeStatic = isSiyuanBuild
 const staticV = "202304191333"
 
 console.log("isDev=>", isDev)
-console.log("appBase=>", appBase)
-console.log("isSiyuanBuild=>", isSiyuanBuild)
+console.log("BUILD_TYPE=>", process.env.BUILD_TYPE)
+console.log("isNodeBuild=>", isNodeBuild)
 console.log("isVercelBuild=>", isVercelBuild)
+console.log("isStaticBuild=>", isStaticBuild)
+console.log("isSiyuanBuild=>", isSiyuanBuild)
 console.log("isSsr=>", isSsr)
-// console.log("ssrPreset=>", ssrPreset)
-// console.log("ssrServeStatic=>", ssrServeStatic)
+console.log("ssrPreset=>", ssrPreset)
+console.log("ssrServeStatic=>", ssrServeStatic)
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
@@ -74,16 +107,13 @@ export default defineNuxtConfig({
   // https://nuxt.com/docs/guide/going-further/custom-routing#hash-mode-spa
   router: {
     options: {
-      hashMode: true,
+      hashMode: !isSsr,
     },
   },
   nitro: {
-    output: {
-      publicDir: distDir,
-    },
-    //   preset: ssrPreset,
-    //   // 开启之后将进行静态伺服
-    //   serveStatic: ssrServeStatic,
+    preset: ssrPreset,
+    // 开启之后将进行静态伺服
+    serveStatic: ssrServeStatic,
   },
   meilisearch: {
     hostUrl: process.env.MEILISEARCH_ENDPOINT ?? "http://localhost:3000/api/endpoint/meilisearch",
