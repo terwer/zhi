6c6c02a90585f1d928b34e6ca1e45e10a69a058c
terwer <youweics@163.com>
2023-05-14 13:22:03 +0800

    feat(zhi-docs-vitepress): add vitepress docs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,4 @@
 module.exports = {
   root: true,
-  extends: ["./node_modules/@terwer/eslint-config-custom/index.cjs"],
+  extends: ["./node_modules/@terwer/eslint-config-custom/typescript/index.cjs"],
 }
