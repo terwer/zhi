8a403cd4363fbb2aec432567bdeb00b83970f57c
terwer <youweics@163.com>
2023-05-16 13:18:21 +0800

    feat(zhi-publisher-sdk): add publisher sdk

--- libs/zhi-publisher-sdk/.prettierrc.cjs
+++ libs/zhi-publisher-sdk/.prettierrc.cjs
@@ -23,26 +23,8 @@
  * questions.
  */
 
-/**
- * 博客类型枚举
- *
- * @author terwer
- * @version 1.0.0
- * @since 1.0.0
- */
-enum BlogTypeEnum {
-  /**
-   * 思源笔记
-   */
-  BlogTypeEnum_Siyuan = "siyuan",
-  /**
-   * Metaweblog
-   */
-  BlogTypeEnum_Metaweblog = "metaweblog",
-  /**
-   * WordPress
-   */
-  BlogTypeEnum_Wordpress = "wordpress",
+module.exports = {
+  semi: false,
+  singleQuote: false,
+  printWidth: 120
 }
-
-export default BlogTypeEnum
