6040f3217fbdbe4bab0c102fc3ca606930dcbc79
terwer <youweics@163.com>
2023-05-16 13:38:05 +0800

    chore: fix ci

--- tools/typedoc-vitepress-theme/jest.config.js
+++
@@ -1,7 +0,0 @@
-const base = require('../../jest.config.base.js');
-
-module.exports = {
-  ...base,
-  name: 'vuepress-plugin-typedoc',
-  displayName: 'vuepress-plugin-typedoc',
-};
--- tools/typedoc-vitepress-theme/src/options.ts
+++
@@ -1,4 +0,0 @@
-export const DEFAULT_SIDEBAR_OPTIONS = {
-  autoConfiguration: true,
-  format: 'vitepress',
-};
