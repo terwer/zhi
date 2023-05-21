760328664748543c978d2bf57dc75ce7b8455ea5
terwer <youweics@163.com>
2023-05-21 22:33:33 +0800

    feat(zhi-root): add hook、siyuanhook、bridge

---
+++ libs/zhi-publisher-hook/src/index.ts
@@ -0,0 +1,4 @@
+import PublishHook from "./publish-hook"
+import { SiyuanDevice, BrowserUtil, DeviceDetection, DeviceTypeEnum } from "zhi-device"
+
+export { PublishHook, SiyuanDevice, BrowserUtil, DeviceDetection, DeviceTypeEnum }
