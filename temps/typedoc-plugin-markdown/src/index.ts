7eb173c07c671eaeb12564fa1383a997a733760f
terwer <youweics@163.com>
2023-06-06 01:32:40 +0800

    docs: update deps

--- temps/typedoc-plugin-markdown/src/index.ts
+++ temps/typedoc-plugin-markdown/src/index.ts
@@ -1,35 +1,35 @@
-import { Application } from 'typedoc';
-import { declareOptions } from './options/options';
-import { generateMarkdown, renderMarkdown } from './renderer/renderer';
-import { MarkdownTheme } from './theme';
+import { Application } from "typedoc"
+import { declareOptions } from "./options/options"
+import { generateMarkdown, renderMarkdown } from "./renderer/renderer"
+import { MarkdownTheme } from "./theme"
 
 export function load(app: Application) {
   /**
    * Exposes markdown and bootstrap the markdown theme to the renderer
    */
-  app.renderer.defineTheme('markdown', MarkdownTheme);
+  app.renderer.defineTheme("markdown", MarkdownTheme)
 
   /**
    * Defines all plugin options
    */
-  declareOptions(app);
+  declareOptions(app)
 
   /**
    * Decouple HTML logic from the renderer (there should probably be a better solution to this)
    */
-  Object.defineProperty(app, 'generateDocs', { value: generateMarkdown });
-  Object.defineProperty(app.renderer, 'render', {
+  Object.defineProperty(app, "generateDocs", { value: generateMarkdown })
+  Object.defineProperty(app.renderer, "render", {
     value: renderMarkdown,
     configurable: true,
-  });
+  })
 }
 
 /**
  * Expose global entrypoints
  */
-export * from './models';
-export * from './options/options-reader';
-export { MarkdownRendererEvent } from './renderer/renderer';
-export { partials } from './resources/resources';
-export { MarkdownTheme } from './theme';
-export { MarkdownThemeRenderContext } from './theme-render-context';
+export * from "./models"
+export * from "./options/options-reader"
+export { MarkdownRendererEvent } from "./renderer/renderer"
+export { partials } from "./resources/resources"
+export { MarkdownTheme } from "./theme"
+export { MarkdownThemeRenderContext } from "./theme-render-context"
