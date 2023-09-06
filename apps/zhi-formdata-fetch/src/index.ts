b3c3b335fae01596d7c8c322c3fd76e52cd961a7
terwer <youweics@163.com>
2023-09-06 14:47:10 +0800

    feat: change form fetch

--- apps/zhi-formdata-fetch/src/index.ts
+++
@@ -1,6 +0,0 @@
-import FormdataFetch from "./lib/formdataFetch"
-import fetch, { Blob, FormData, Headers } from "node-fetch"
-
-export { FormdataFetch }
-export { fetch }
-export { Blob, FormData, Headers }
